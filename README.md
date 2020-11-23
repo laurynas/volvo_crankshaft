89349e4d8b9c6e8ccc87d1fe53cb0b4375cb03a4
Laurynas Butkus <laurynas.butkus@gmail.com>
2020-11-23 20:51:57 +0200

    Add crankshaft service

---
+++ README.md
@@ -0,0 +1,10 @@
+# Volvo V50 + Crankshaft + Raspberry Pi + Arduino = ❤️
+
+## Configure RPi / Crankshaft
+
+Copy files from [crankshaft/](crankshaft/) to RPi.
+
+Enable serial service
+
+> systemctl enable volvo_crankshaft
+> systemctl start volvo_crankshaft 
---
+++ crankshaft/opt/service_volvo_crankshaft.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SERIAL_DEVICE=/dev/ttyACM0
+ANDROID_CONNECTED="+"
+ANDROID_DISCONNECTED="-"
+
+while true; do
+    if [ -c $SERIAL_DEVICE ]; then
+        if [ -f /tmp/android_device ]; then
+            echo $ANDROID_CONNECTED > $SERIAL_DEVICE
+        else
+            echo $ANDROID_DISCONNECTED > $SERIAL_DEVICE
+        fi
+    fi
+
+    sleep 1
+done
+
+exit 0
